91a54fbd37b0c671f14a7f42eb699b0310882cf9
willy0483 <willyjensen251@gmail.com>
2025-06-09 01:00:04 +0200

    CreateSession#7

---
+++ apps/web/src/lib/session.ts
@@ -0,0 +1,52 @@
+"use server";
+import { jwtVerify, SignJWT } from "jose";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export type Session = {
+  user: {
+    id: string;
+    name: string;
+  };
+  // accessToken: string;
+  // refreshToken: string;
+};
+
+const secretKey = process.env.SESSION_SECRET_KEY!;
+const encodedKey = new TextEncoder().encode(secretKey);
+
+export const createSession = async (payload: Session) => {
+  const expiredAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+
+  const session = await new SignJWT(payload)
+    .setProtectedHeader({
+      alg: "HS256",
+    })
+    .setIssuedAt()
+    .setExpirationTime(expiredAt)
+    .sign(encodedKey);
+
+  (await cookies()).set("session", session, {
+    httpOnly: true, // Cookie is not accessible via JavaScript (helps prevent XSS)
+    secure: true, // Cookie is only sent over HTTPS
+    expires: expiredAt, // Cookie expiration date
+    sameSite: "lax", // Restricts cookie to same-site requests (mitigates CSRF)
+    path: "/", // Cookie is valid for the entire site
+  });
+};
+
+export const getSession = async () => {
+  const cookie = (await cookies()).get("session")?.value;
+  if (!cookie) return null;
+
+  try {
+    const { payload } = await jwtVerify(cookie, encodedKey, {
+      algorithms: ["HS265"],
+    });
+
+    return payload as Session;
+  } catch (error) {
+    console.error("Failed to verify the session", error);
+    redirect("/auth/signin");
+  }
+};
